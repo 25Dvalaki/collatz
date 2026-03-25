9d6d7fc0f82a5e7feefc4404375a5f6249c776db
25Dvalaki <231025431+25Dvalaki@users.noreply.github.com>
2026-03-25 11:25:39 +0100

    img

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,9 @@ function collatz() {
 
   // get integer, absolute value it so you dont do a recursive memleak
   var n = Math.abs(parseInt(document.getElementById("szam").value));
-  let k = ""; // write the results all at once (set later)
+  let k = ""; // result writing
   
-  // crash prevention hhhh
+  // crash prevention
   if (isNaN(n) || n == 0) {
     document.getElementById("result").innerHTML = "Adjon meg egy nem-nulla értéket!";
     return;
@@ -20,14 +20,18 @@ function collatz() {
     } else {
       n = 3 * n + 1;
     }
+
+    /** not sure if toString() is necessary because
+    * JS has like no typesafety but who cares, trying
+    * C has given me sleep paralysis demons
+    */
+
     if (n != 1) {
     k = k + n.toString() + ", ";
     } else {
-      k = k + n.toString();
+    k = k + n.toString();
     }
-    // "i swear i debugged for more than 4 tests" with clear syntax errors in debug prints
-    // console.debug(k);
-    // console.debug(n);
+  
     i++;
   }
 
