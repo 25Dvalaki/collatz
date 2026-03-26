c05170bd75be9f4f797764a706c1a7eaa0db3c97
25Dvalaki <231025431+25Dvalaki@users.noreply.github.com>
2026-03-26 10:00:30 +0100

    just shorten this further

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,38 @@
 // appends the value of n (as string) to k
-// felt like making stuff more readable so this exists
+// "string merge listing"
 function strmrgls(a, b) {
   // C gave me sleep paralysis demons, toString() go
   a = a + b.toString() + (b != 1 ? ", " : "");
   return a;
 }
 
+// extra shorter
+function byID(a) {
+  return document.getElementById(a);
+}
+
 function collatz() {
 
   // get input as integer, absolute value in case negative
-  var n = Math.abs(parseInt(document.getElementById("szam").value));
+  var n = Math.abs(parseInt(byID("szam").value));
   let k = ""; // listing
+  var i = 0; // amount of times ran
   
-  // crash prevention
+  // crash prevention (0 & no input)
   if (isNaN(n) || n == 0) {
-    document.getElementById("result").innerHTML = "Adjon meg egy nem-nulla értéket!";
+    byID("result").innerHTML = "Adjon meg egy nem-nulla értéket!";
     return;
   }
 
-  // amount of times ran
-  var i = 0;
-
   while (n != 1) {
-    // do theory or something idk
-    n = (n % 2 == 0 ? n/2 : 3*n+1);
+    n = (n % 2 == 0 ? n/2 : 3*n+1); // the theory
 
-    k = strmrgls(k, n);
+    k = strmrgls(k, n); // merge
 
-    // +1 run
     i++;
   }
 
   // output result
-  document.getElementById("result").innerHTML = i + " eséllyel futódott le, értékek:";
-  document.getElementById("results").innerHTML = k;
+  byID("result").innerHTML = i + " eséllyel futódott le, értékek:";
+  byID("results").innerHTML = k;
 }
