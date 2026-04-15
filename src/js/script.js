8709b8f0282832a3478ebc426af2d456a5f62907
25Dvalaki <231025431+25Dvalaki@users.noreply.github.com>
2026-04-15 10:17:09 +0200

    final random change i felt like doing

--- src/js/script.js
+++ src/js/script.js
@@ -36,7 +36,7 @@ function collatz() {
   }
 
   while (n != 1) {
-    n = (n % 2 == 0 ? n/2 : 3*n+1); // the theory
+    n = (!(n % 2) ? n/2 : 3*n+1); // the theory
 
     k = strmrgls(k, n); // merge
 
